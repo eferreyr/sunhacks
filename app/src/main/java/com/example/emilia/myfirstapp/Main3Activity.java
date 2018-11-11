ecec72af8f776f624c3d56566e09c1f1042da7f0
lera.star <valeriiastarosek@gmail.com>
2018-11-10 22:41:43 -0700

    did the logic (kinda)

--- app/src/main/java/com/example/emilia/myfirstapp/Main3Activity.java
+++ app/src/main/java/com/example/emilia/myfirstapp/Main3Activity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class Main3Activity extends AppCompatActivity {
@@ -14,9 +15,44 @@ public class Main3Activity extends AppCompatActivity {
     ListView myListView;
     String[] items;
     String[] descriptions;
-    FoodPlace BarrettDiningCenter = new FoodPlace();
 
-    //@Override
+    FoodPlace BarrettDiningCenter = new FoodPlace("Barrett Dining Center");
+    FoodPlace Hassayampa = new FoodPlace("Hassayampa");
+    FoodPlace ManzanitaDining = new FoodPlace("Manzanita Dining");
+    FoodPlace Pitchforks = new FoodPlace("Pitchforks");
+    FoodPlace TookerHouseDining = new FoodPlace("Tooker House Dining");
+    FoodPlace AFC = new FoodPlace("AFC");
+    FoodPlace BurgerKing = new FoodPlace("Burger King");
+    FoodPlace ChickFilA = new FoodPlace("Chick-fil-A");
+    FoodPlace ChickIn = new FoodPlace("Chick-In");
+    FoodPlace DevilsGreens = new FoodPlace("Devil\'s Greens");
+    FoodPlace DogsHouse = new FoodPlace("DogsHouse");
+    FoodPlace Dominos = new FoodPlace("Domino\'s");
+    FoodPlace EinsteinBrosBagels = new FoodPlace("Einstein Bros Bagels");
+    FoodPlace Engrained = new FoodPlace("Engrained");
+    FoodPlace Freshii = new FoodPlace("Freshii");
+    FoodPlace JambaJuice = new FoodPlace("Jamba Juice");
+    FoodPlace Qdoba = new FoodPlace("Qdoba");
+    FoodPlace PeiWei = new FoodPlace("Pei Wei");
+    FoodPlace Subway = new FoodPlace("Subway");
+    FoodPlace TheCrepeClub = new FoodPlace("The Crepe Club");
+    FoodPlace DonerDudes = new FoodPlace("DonerDudes");
+    FoodPlace RosiesAtMU = new FoodPlace("RosiesAtMU");
+    FoodPlace BurningBCafes = new FoodPlace("BurningBCafes");
+    FoodPlace CharliesCafe = new FoodPlace("Charlie\'s Cafe");
+    FoodPlace Starbucks = new FoodPlace("Starbucks");
+    FoodPlace SonoraMarket = new FoodPlace("Sonora Market");
+    FoodPlace PodMarket = new FoodPlace("POD Market");
+
+    FoodPlace[] placesList = {BarrettDiningCenter, Hassayampa, ManzanitaDining, Pitchforks, TookerHouseDining, AFC, BurgerKing, ChickFilA,
+            ChickIn, DevilsGreens, DogsHouse, Dominos, EinsteinBrosBagels, Engrained, Freshii, JambaJuice, Qdoba, PeiWei,
+            Subway, TheCrepeClub, DonerDudes, RosiesAtMU, BurningBCafes, CharliesCafe, Starbucks, SonoraMarket, PodMarket};
+
+    ArrayList foodToRecommend = new ArrayList();
+
+    //addressing an object RestPref
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recommendations);
@@ -52,4 +88,28 @@ public class Main3Activity extends AppCompatActivity {
             }
         });
     }
+
+
+public FoodPlace[] recommendPlaces(RestPref restPref){
+    for(int i = 0; i < placesList.length; i++){
+        if((restPref.isVeg().equals(placesList[i].isVegetarian()) &&
+                (restPref.isVegan().equals(placesList[i].isVegan()) &&
+                (restPref.isGF().equals(placesList[i].isGlutenFree()) &&
+                (restPref.likesJap().equals(placesList[i].isJapanese()) &&
+                (restPref.likesChin().equals(placesList[i].isChinese()) &&
+                (restPref.likesInd().equals(placesList[i].isIndian()) &&
+                (restPref.likesMed().equals(placesList[i].isMediterranean()) &&
+                (restPref.likesPiz().equals(placesList[i].isPizza()) &&
+                (restPref.likesBG().equals(placesList[i].isBakedGoods()) &&
+                (restPref.likesTexmex().equals(placesList[i].isMexican()) &&
+                (restPref.likesChick().equals(placesList[i].isChicken()) &&
+                (restPref.likesBurg().equals(placesList[i].isBurgers()) &&
+                (restPref.payCheap().equals(placesList[i].price) &&
+                (restPref.payMid().equals(placesList[i].price) &&
+                (restPref.payExpen().equals(placesList[i].price)){
+
+        }
+
+    }
 }
+
